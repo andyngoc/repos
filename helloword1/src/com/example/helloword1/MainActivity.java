377ce1b3ac72ff3593d0e5a8f9c7bf46fe5df8af
thuydtn <thuydtn@aureole-it.vn>
2013-03-27 10:20:55 +0700

    a

--- helloword1/src/com/example/helloword1/MainActivity.java
+++ helloword1/src/com/example/helloword1/MainActivity.java
@@ -5,7 +5,7 @@ import android.app.Activity;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
-
+/*commennt 0*/
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
